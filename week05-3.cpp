eb3d4a87f3acb3c1e59d069ea0a6d884768e649a
kamikoprint <114201502+kamikoprint@users.noreply.github.com>
2022-10-07 10:23:47 +0800

    05

---
+++ week05-3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    ///int a=10;
+    ///while(a>0){
+       /// printf("a:%d a>0\n", a);
+        ///a--;
+        ///}
+
+    for( int a=5; a>0; a--){
+        printf("a:%d a>0\n",a);
+    }
+
+return 0;
+
+}
