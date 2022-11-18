58492a32f6e9eed84865bec3477cb35aaabe8b9d
kamikoprint <ryan997722@gmail.com>
2022-11-18 11:49:09 +0800

    week11

---
+++ week06/week06-3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    printf("清輸入2個數: ");
+    int a, b, ans;
+    scanf("%d %d", &a, &b);
+
+
+
+    for(int i=1; i<=a; i++){
+        if( a%i==0 && b%i==0) ans=i;
+        }
+
+    printf("找到ans:%d", ans);
+
+return 0;
+}
